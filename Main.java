ea5f0b6372680e305fdf03387ec9f7284b2fed0c
balez4464 <balez4464@wrdsb.ca>
2020-10-07 13:17:19 +0000

    Made a program that prints users star count into a box

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program prints out how mnay stars the user wants on seprate lines times thier number
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +11,28 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates scanner
+    Scanner input = new Scanner(System.in);
+    
+    //asks user for number of stars
+    System.out.println("Please enter a number between 1 and 10");
+    //users number
+    int usersNumber = input.nextInt();
+    //variable for star
+    String star = ("*");
+    int columns = usersNumber;
+    
+    //for loop to count up printing stars
+    for (int firstCounter = 1; firstCounter <= usersNumber; firstCounter++){
+        //prints new line for diferent rows
+        System.out.println();
+        //allows fot columns
+        for (int secondCounter = 1; secondCounter <= columns; secondCounter++){
+          System.out.print(star + " ");
+        }
+        
+    }
+    
     
   }
 }
